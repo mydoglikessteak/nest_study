5a1d8aa25e541181a33a05a5b1083bc30c9755b7
Vini35 <viniciusalex0303@gmail.com>
2022-03-16 17:52:10 -0300

    Update

---
+++ src/task/task.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('task')
+export class TaskController {}
+// for generate componnets like controller and modules in the same module
+// always remember to put the same name on nest g.
