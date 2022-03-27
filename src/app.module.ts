0439c613d587281722590a5d00c6f72842a28f24
Vini35 <viniciusalex0303@gmail.com>
2022-03-27 14:10:45 -0300

    added ps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './tasks/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronous: true,
+    }),
+  ],
 })
 export class AppModule {}
